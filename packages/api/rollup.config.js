5a09ca91ad0cdcd3da19880afba9aac56c1c845b
Jose E. Bolanos <jenrique7@gmail.com>
2022-10-20 23:36:10 -0600

    fix(api): build using rollup

---
+++ packages/api/rollup.config.js
@@ -0,0 +1,12 @@
+const resolve = require('@rollup/plugin-node-resolve');
+const commonjs = require('@rollup/plugin-commonjs');
+const json = require('@rollup/plugin-json');
+
+module.exports = {
+  input: 'out-tsc/index.js',
+  output: {
+    file: 'dist/index.js',
+    format: 'cjs',
+  },
+  plugins: [resolve(), commonjs(), json()],
+};
